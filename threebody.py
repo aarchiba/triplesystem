2d53b79cac4d58dd421111d8ce20778f6840f82e
Anne Archibald <peridot.faceted@gmail.com>
2013-03-29 06:19:46 -0400

    Used Einstein delay to carry out fitting
    Now get ~ 2.5 us residuals.

--- threebody.py
+++ threebody.py
@@ -88,7 +88,12 @@ def process_remove_trend(o):
     o["states"][:,2] = remove_trend(o["states"][:,2])
 
 def compute_orbit(parameters, times, with_derivatives = False, epoch = 0,
-        save_orbits=False, load_orbits=False, tol=1e-16):
+        save_orbits=False, load_orbits=False, tol=1e-16, 
+        shapiro=False, special=False, general=False):
+    if (load_orbits or save_orbits) and (shapiro or special or general1 or general2):
+        raise NotImplementedError
+    if with_derivatives and (special or general):
+        raise NotImplementedError
     parameters = np.asarray(parameters)
     times = np.asarray(times)
     if load_orbits:
@@ -98,6 +103,7 @@ def compute_orbit(parameters, times, with_derivatives = False, epoch = 0,
 
     o = dict(parameters=parameters, times=times)
 
+    # FIXME: deal with bogus values when state is 22 long
     try:
         initial_values, jac = kepler.kepler_three_body_measurable(
                 *(list(parameters)+[0,np.zeros(3),np.zeros(3),0]))
@@ -116,10 +122,16 @@ def compute_orbit(parameters, times, with_derivatives = False, epoch = 0,
     
     # FIXME: deal with epoch not at the beginning
 
+    if special or general:
+        rhs = quad_integrate.KeplerRHSRelativity(
+                special=special, general=general)
+        initial_values = np.concatenate((initial_values, [0]))
+    else:
+        rhs = quad_integrate.KeplerRHS()
     states = []
     if with_derivatives:
         derivatives = []
-        O = quad_integrate.ODE(quad_integrate.KeplerRHS(),
+        O = quad_integrate.ODE(rhs,
                initial_values, 0,
                rtol = tol, atol = tol,
                vectors = vectors,
@@ -130,7 +142,7 @@ def compute_orbit(parameters, times, with_derivatives = False, epoch = 0,
             derivatives.append(O.dx)
         derivatives = np.array(derivatives)
     else:
-        O = quad_integrate.ODE(quad_integrate.KeplerRHS(),
+        O = quad_integrate.ODE(rhs,
                initial_values, 0,
                rtol = tol, atol = tol,
                vectors = [],
@@ -143,6 +155,10 @@ def compute_orbit(parameters, times, with_derivatives = False, epoch = 0,
 
     o["states"] = states
     o["derivatives"] = derivatives
+    if special or general:
+        o["einstein"] = states[:,21]
+    if shapiro:
+        o["shapiro"] = shapiros(states)
 
     if save_orbits:
         o["name"] = save_orbit(**o)
