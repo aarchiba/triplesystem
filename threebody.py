48970948dbdbba0264411e7019aaea9c457960f7
Anne Archibald <peridot.faceted@gmail.com>
2013-04-01 23:54:14 -0400

    compute_orbit cleanup

--- threebody.py
+++ threebody.py
@@ -70,56 +70,6 @@ best_errors = [
 	7.981798704760887e-06 ,
 ]
 
-
-orbit_dir = "orbits"
-
-def save_orbit(parameters, times, states, derivatives = None):
-    try:
-        os.mkdir(orbit_dir)
-    except OSError:
-        pass
-
-    name = time.strftime("%Y-%m-%d-%H:%M:%S")
-    name += "-"+"".join([random.choice(string.lowercase) for i in range(20)])
-    name = os.path.join(orbit_dir,name)
-
-    np.savez(name, 
-            parameters = parameters,
-            times = times,
-            states = states,
-            derivatives = derivatives)
-    return name
-
-def saved_orbits(times = None, with_derivatives = False):
-    for n in sorted(glob(os.path.join(orbit_dir, "*-*-*-*:*:*-*.npz")))[::-1]:
-        r = {"name":n}
-        with np.load(n) as f:
-            if with_derivatives and "derivatives" not in f:
-                continue
-            if times is not None and not np.all(times==f["times"]):
-                continue
-            for a in f.files:
-                r[a] = f[a]
-        yield r
-
-def best_orbit(quality_evaluator, post_process = None, 
-        times = None,
-        with_derivatives = False):
-    r = np.inf
-    best = None
-    for o in saved_orbits(times, with_derivatives):
-        if post_process is not None:
-            post_process(o)
-        nr = quality_evaluator(o)
-        if nr<r:
-            r = nr
-            best = o
-    return best
-
-def least_squared(delays):
-    def _least_squared(o):
-        return np.mean((o.states[:,2]-delays)**2)
-    return _least_squared
 def remove_trend(vec, mjds, tel_list, tels, uncerts=None,
     P=True, Pdot=True, jumps=True,
     position=False, proper_motion=False, parallax=False):
@@ -150,25 +100,18 @@ def remove_trend(vec, mjds, tel_list, tels, uncerts=None,
             vec/uncerts)
     return vec-np.dot(non_orbital_basis,x)
 
-def process_remove_trend(o):
-    o["states"][:,2] = remove_trend(o["states"][:,2])
-
-def compute_orbit(parameters, times, with_derivatives = False, epoch = 0,
-        save_orbits=False, load_orbits=False, tol=1e-16, 
+def compute_orbit(parameters, times, with_derivatives=False, epoch=0, tol=1e-16, delta=1e-6,
         shapiro=False, special=False, general=False, use_quad=False):
-    if (load_orbits or save_orbits) and (shapiro or special or general1 or general2):
-        raise NotImplementedError
-    if shapiro and with_derivatives:
-        raise NotImplementedError
+    # FIXME: deal with epoch not at the beginning
+
     start = time.time()
+
     parameters = np.asarray(parameters)
     times = np.asarray(times)
-    if load_orbits:
-        for o in saved_orbits(times, with_derivatives):
-            if np.all(o["parameters"]==parameters):
-                return o
 
-    o = dict(parameters=parameters, times=times)
+    o = dict(parameters=parameters, times=times, tol=tol,
+            shapiro=shapiro, special=special, general=general,
+            use_quad=use_quad)
 
     try:
         initial_values, jac = kepler.kepler_three_body_measurable(
@@ -178,73 +121,110 @@ def compute_orbit(parameters, times, with_derivatives = False, epoch = 0,
             ls = 22
         else:
             ls = 21
-        states = np.random.uniform(0, 1e40, (len(times),ls))
-        o['states'] = states
+        o['states'] = np.random.uniform(0, 1e40, (len(times),ls))
+        o['delays'] = np.random.uniform(0, 1e40, len(times))
+        if shapiro:
+            o['shapiro'] = np.random.uniform(0, 1e40, len(times))
         if with_derivatives:
-            derivatives = np.random.normal(0, 1e40, (len(times),ls,14))
-            o['derivatives'] = derivatives
+            o['derivatives'] = np.random.normal(0, 1e40, (len(times),14))
         o['name'] = None
         return o
     vectors = jac[:,:14].T
     
-    # FIXME: deal with epoch not at the beginning
-
-    rhs = quad_integrate.KeplerRHS(
-            special=special, general=general)
+    rhs = quad_integrate.KeplerRHS(special=special, general=general)
     if special or general:
         initial_values = np.concatenate((initial_values, [0]))
         vectors = np.concatenate([vectors, np.zeros(14)[:,None]], axis=1)
     states = []
+    shapiro_delays = []
     if with_derivatives:
         derivatives = []
         O = quad_integrate.ODE(rhs,
                initial_values, 0,
                rtol = tol, atol = tol,
                vectors = vectors,
-               delta = 1e-10,
+               delta = delta,
                use_quad = use_quad)
         for t in times:
             O.integrate_to(t)
             states.append(O.x)
-            derivatives.append(O.dx)
+            dx = O.dx
+            derivatives.append(dx[2,:].copy())
+            if special or general:
+                derivatives[-1] += 86400*dx[21,:]
+            if shapiro:
+                s, d = shapiros(O.x, with_derivatives=True)
+                derivatives[-1] += np.dot(d, dx)
+                shapiro_delays.append(s)
         derivatives = np.array(derivatives)
     else:
         O = quad_integrate.ODE(rhs,
                initial_values, 0,
                rtol = tol, atol = tol,
                vectors = [],
-               delta = 1e-10,
                use_quad = use_quad)
         for t in times:
             O.integrate_to(t)
             states.append(O.x)
-        derivatives = None
+            if shapiro:
+                s = shapiros(O.x)
+                shapiro_delays.append(s)
     states = np.array(states)
 
     o["states"] = states
-    o["derivatives"] = derivatives
+    o["delays"] = states[:,2].copy()
+    if with_derivatives:
+        o["derivatives"] = derivatives
     if special or general:
-        o["einstein"] = states[:,21]
+        o["einstein_delays"] = states[:,21]
+        o["delays"] += 86400*states[:,21]
+        # einstein already included in derivatives
     if shapiro:
-        o["shapiro"] = shapiros(states)
+        o["shapiro_delays"] = np.array(shapiro_delays)
+        o["delays"] += o["shapiro_delays"]
+
     o["n_evaluations"] = O.n_evaluations
     o["time"] = time.time()-start
 
-    if save_orbits:
-        o["name"] = save_orbit(**o)
-
     return o
 
-def shapiro_delay(s_src, s_m):
+def shapiro_delay(s_src, s_m, with_derivatives=False):
+    s_src = np.asarray(s_src)
+    s_m = np.asarray(s_m)
     c = 86400. # lt-s per day
     dx = s_m[...,:3]-s_src[...,:3]
     ldx = np.sqrt(np.sum(dx**2,axis=-1))
-    #return -86400*2*kepler.G*s_m[...,6]*c**(-3)*np.log(1-dx[...,2]/ldx) # in s
-    return -86400*2*kepler.G*s_m[...,6]*c**(-3)*np.log(dx[...,2]+ldx) # in s
-
-def shapiros(states):
-    return (shapiro_delay(states[:,:7],states[:,7:14]) +
-                shapiro_delay(states[:,:7],states[:,14:21]))
+    cst = -86400*2*kepler.G*c**(-3)
+    r=cst*s_m[...,6]*np.log(dx[...,2]+ldx) # in s
+    if with_derivatives:
+        dz = np.zeros(s_src.shape[:-1]+(14,))
+        dz[...,2] = -1
+        dz[...,9] = 1
+        dldx = np.zeros(s_src.shape[:-1]+(14,))
+        dldx[...,:3] = -dx/ldx[...,None]
+        dldx[...,7:10] = dx/ldx[...,None]
+        d = (s_m[...,6]/(dx[...,2]+ldx))[...,None]*(dz+dldx)
+        d[...,-1] = np.log(dx[...,2]+ldx)
+        d *= cst
+        return r, d
+    else:
+        return r
+    
+def shapiros(states, with_derivatives=False):
+    states = np.asarray(states)
+    if with_derivatives:
+        r1, d1 = shapiro_delay(states[...,:7],states[...,7:14], 
+            with_derivatives=True)
+        r2, d2 = shapiro_delay(states[...,:7],states[...,14:21], 
+            with_derivatives=True)
+        d = np.zeros_like(states)
+        d[...,:14] += d1
+        d[...,:7] += d2[...,:7]
+        d[...,14:21] += d2[...,7:14]
+        return r1+r2, d
+    else:
+        return (shapiro_delay(states[...,:7],states[...,7:14]) +
+                shapiro_delay(states[...,:7],states[...,14:21]))
 
 def fmt(x, u):
     
