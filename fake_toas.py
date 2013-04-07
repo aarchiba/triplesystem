fb9872ef2ef716fa4ba331a75eabe58f6c4538bc
Anne Archibald <peridot.faceted@gmail.com>
2013-04-07 01:23:49 -0400

    Adjust times to pulse emission times before integration

--- fake_toas.py
+++ fake_toas.py
@@ -27,28 +27,31 @@ rms = np.sqrt(np.mean((odelay+lindelay-delays)**2))
 #    print n, repr(p)
 #print np.sqrt(np.mean((odelay-delays+np.dot(lin,lin_parameters))**2))
 
-#times = mjds
 days = 1000
 n_per_day = 100
-times = np.linspace(0,days,n_per_day*days+1)
-o = threebody.compute_orbit(threebody.best_parameters,times)
-lin, lin_names = threebody.trend_matrix(times, None, None, jumps=False)
-lindelay = np.dot(lin,lin_parameters[:len(lin_names)])
+times = np.linspace(1,days+1,n_per_day*days+1)
+#times = mjds
 
 def phase(t):
     dt = (t-(pepoch-mjdbase))*86400
     return dt*f0 + 0.5*dt**2*f1
+for i in range(len(times)):
+    t = times[i]
+    pulse = np.round(phase(t))
+    dt = (t-(pepoch-mjdbase))*86400
+    t -= ((phase(t)-pulse)/(f0+dt*f1))/86400
+    # now t is a pulse emission time
+    times[i] = t
+
+o = threebody.compute_orbit(threebody.best_parameters,times)
+lin, lin_names = threebody.trend_matrix(times, None, None, jumps=False)
+lindelay = np.dot(lin,lin_parameters[:len(lin_names)])
+
 with open("fake.tim","wt") as tim:
     with open("fake.pulses","wt") as pulses:
         for i in range(len(o["times"])):
 
             t = o["times"][i]
-            print phase(t), 
-            pulse = np.round(phase(t))
-            dt = (t-(pepoch-mjdbase))*86400
-            t -= ((phase(t)-pulse)/(f0+dt*f1))/86400
-            print phase(t)
-            # now t is a pulse emission time
             d = o["delays"][i] + lindelay[i]
             t += d/86400
             # now t is a barycentered arrival time
