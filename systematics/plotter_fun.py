30f05e349ade6a78cecac28663cd2dae4e9358f2
nina <nina-neiman@yandex.ru>
2017-10-06 18:52:13 +0200

    update functions and README

--- systematics/plotter_fun.py
+++ systematics/plotter_fun.py
@@ -8,10 +8,14 @@ import lsqr_fit_fun as lsqr_fit
 
 #function-3---create_plotable_arrows
 ####################################
-plotter_out = namedtuple("plotter_out",
-    ["X","Y","U","V","M","err_X","err_Y","err_ix"])
 
 def plotter(par_dict, Acols, mjd, phase, unc):
+    '''Calculates arrow coefficients and values for a given data set.
+       Returns: object with arrows (coordinates, directions and lengths as well as 1-sigma errors) 
+par_dict - dictionary with pulsar parameters
+Acols - max number of inner orbital frequencies to calculate
+mjd, phase, unc - parameters of data''' 
+
     ang = np.linspace(0,2*np.pi,100)
     circle = np.array([np.cos(ang),np.sin(ang)])
     fit = lsqr_fit.linear_least_squares_cov(par_dict, Acols, mjd, phase, unc)
@@ -46,4 +50,27 @@ def plotter(par_dict, Acols, mjd, phase, unc):
             err_Y.append(Lcircle[1])
 
     M = np.hypot(V, U)
-    return plotter_out(X=X, Y=Y, U=U, V=V, M=M, err_X=err_X, err_Y=err_Y, err_ix=err_ix)   
+    class Result(object):
+         pass
+    pl = Result()
+    pl.X = np.array(X)
+    pl.Y = np.array(Y)
+    pl.U = np.array(U)
+    pl.V = np.array(V)
+    pl.M = np.array(M)
+    pl.err_X = np.array(err_X)
+    pl.err_Y = np.array(err_Y)
+    pl.err_ix = np.array(err_ix)
+    return pl
+
+
+def arrow_extract(pl,k,j):
+    '''Takes (arrow_coeff) object and frequecncies of the arrow you want to get info about.
+       Returns len=3 array: [projection_X, projection_Y, lenght].
+pl - object with arrow coordinates, directions and lenghts
+k - inner orbit frequency
+j - outer orbit frequency''' 
+    for i in range(0,len(pl.X)):
+            if pl.X[i]==k and pl.Y[i]==j:
+                ll=np.array([pl.U[i], pl.V[i], pl.M[i]])
+    return ll
