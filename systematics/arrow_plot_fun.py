8f0c5ccac5fd593c7839f1de41c5db89b604796e
nina <nina-neiman@yandex.ru>
2017-10-06 20:10:00 +0200

    Add units string option

--- systematics/arrow_plot_fun.py
+++ systematics/arrow_plot_fun.py
@@ -17,16 +17,16 @@ err_red=np.array([191, 54, 12])/255.0
 err_bl=np.array([21, 101, 192])/255.0
 
 
-def draw_plot(par_dict, Acols, mjd, phase, unc, scale=None, plot_unc=True, color=err_bl):
+def draw_plot(par_dict, Acols, mjd, phase, unc, scale=None, plot_unc=True, color=err_bl, units=''):
     '''Makes arrow plot for a given data set.
        Returns: value of the biggest arrow and plot
 par_dict - dictionary with pulsar parameters
 Acols - max number of inner orbital frequencies to calculate
 mjd, phase, unc - parameters of data (in units you want to get your arrow lenghts in)'''
     pl=pltr.plotter(par_dict, Acols, mjd, phase, unc)
-    return coeff_plot(pl,scale=scale, plot_unc=plot_unc, color=color)
+    return coeff_plot(pl,scale=scale, plot_unc=plot_unc, color=color, units=units)
 
-def coeff_plot(pl, scale=None, plot_unc=True, color=err_bl):
+def coeff_plot(pl, scale=None, plot_unc=True, color=err_bl, units=''):
     '''Makes arrow plot for a given (arrow_coeff) object.
        Takes: (arrow_coeff) object. 
        Returns: value of the biggest arrow in input units and draws plot
@@ -47,6 +47,6 @@ pl - object with arrow coordinates, directions and lenghts (input your units)'''
     plt.ylabel('Outer orbit frequencies')
     plt.gca().set_aspect('equal')
     err_red=np.array([191, 54, 12])/255.0
-    plt.figtext(0.15,0.2, r'the longest arrow = %f $\mu$s'%(ar_scale), fontproperties=font0, color=err_red)
+    plt.figtext(0.15,0.2, r'the longest arrow = %f %s'%(ar_scale,units), fontproperties=font0, color=err_red)
     return ar_scale
 
